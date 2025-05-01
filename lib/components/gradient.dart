bc3c71deaddb723837a7d81846ab98e3abe8590e
Oumaima-Lg <laghjibioumaima2003@gmail.com>
2025-05-01 21:39:02 +0100

    Welcome

---
+++ lib/components/gradient.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class GradientComponent {
+  static ElevatedButton gradientButton(String text) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+      ),
+      onPressed: () {
+        // Action when button is pressed
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF4023D7), Color(0xFF983BCB)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        constraints: BoxConstraints(
+          maxWidth: 220,
+          maxHeight: 50,
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          text,
+          style: TextStyle(
+            // fontSize: 18,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  static Positioned gradientCircle(
+      double top, double right, Color color1, Color color2) {
+    return Positioned(
+      top: top,
+      right: right,
+      child: Container(
+        width: 70,
+        height: 70,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color1,
+              color2,
+            ],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/welcome.dart
@@ -0,0 +1,87 @@
+import 'package:fitness/components/gradient.dart';
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(bottom: 10, top: 10),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF2E2F55),
+              Color(0xFF23253C),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              // margin: EdgeInsets.only(top: 80),
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 1.95,
+              //   decoration:
+              //       BoxDecoration(border: Border.all(color: Colors.white)),
+              child: Stack(
+                children: [
+                  GradientComponent.gradientCircle(
+                      10, 90, Color(0xFFFFB19A), Color(0xFFFF4902)),
+                  GradientComponent.gradientCircle(
+                      20, 175, Color(0xFF3A3464), Color(0xFF1B1C2A)),
+                  GradientComponent.gradientCircle(
+                      150, 30, Color(0xFF3A3464), Color(0xFF1B1C2A)),
+                  GradientComponent.gradientCircle(
+                      255, 60, Color(0xFF3A3464), Color(0xFF1B1C2A)),
+                  GradientComponent.gradientCircle(
+                      255, 20, Color(0xFFE8ACFF), Color(0xFF7800FF)),
+                  Container(
+                    margin: EdgeInsets.only(top: 75, left: 35, right: 35),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Color(0xFFE8ACFF).withAlpha(51), width: 2),
+                      borderRadius: BorderRadius.circular(41),
+                      color: Color(0xFF4023D7).withAlpha(38),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                          'Without health, \neverything else loses its meaning. \nTake care of it, \nyour health is the key to everything.',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          textAlign: TextAlign.center),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            GradientComponent.gradientButton('Next'),
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: Image.asset(
+                'images/fitness.png',
+                // fit: BoxFit.cover,
+                width: MediaQuery.of(context).size.width,
+                //   height: MediaQuery.of(context).size.height / 4,
+                // height: 200,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
