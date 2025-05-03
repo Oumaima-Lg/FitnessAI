8545723357598817ccf18bb7d2af1659388a9afd
Oumaima-Lg <laghjibioumaima2003@gmail.com>
2025-05-03 09:49:21 +0100

    Delete routes

--- lib/main.dart
+++ lib/main.dart
@@ -25,23 +25,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fitness App',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => ExercicePage(),
-        '/jumping_jack': (context) => JumpingJackPage(), 
-        '/wall_sit': (context) => WallSit(), 
-        '/push_ups': (context) => PushUps(), 
-        '/crunches': (context) => Crunches(), 
-        '/steps_ups': (context) => StepsUps(), 
-        '/squats': (context) => Squats(), 
-        '/tricep_dips_using_a_chair': (context) => TricepDipsUsingAChair(), 
-        '/plank': (context) => Plank(), 
-        '/high_knees': (context) => HighKnees(), 
-        '/forward_lunges': (context) => ForwardLunges(), 
-        '/t_push_ups': (context) => TPushUps(), 
-        '/side_plank': (context) => SidePlank(), 
-      },
+      home: ExercicePage(),
+      // initialRoute: '/',
+      // routes: {
+      //   '/': (context) => ExercicePage(),
+      //   '/jumping_jack': (context) => JumpingJackPage(),
+      //   '/wall_sit': (context) => WallSit(),
+      //   '/push_ups': (context) => PushUps(),
+      //   '/crunches': (context) => Crunches(),
+      //   '/steps_ups': (context) => StepsUps(),
+      //   '/squats': (context) => Squats(),
+      //   '/tricep_dips_using_a_chair': (context) => TricepDipsUsingAChair(),
+      //   '/plank': (context) => Plank(),
+      //   '/high_knees': (context) => HighKnees(),
+      //   '/forward_lunges': (context) => ForwardLunges(),
+      //   '/t_push_ups': (context) => TPushUps(),
+      //   '/side_plank': (context) => SidePlank(),
+      // },
     );
   }
-
 }
